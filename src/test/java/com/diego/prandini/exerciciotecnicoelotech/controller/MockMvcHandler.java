be723eab123e9f11ff3fb051b974c916864abe47
diego.prandini <contato.diego.prandini@gmail.com>
2023-03-02 18:57:41 -0300

    Teste criar Pessoa pela Api

---
+++ src/test/java/com/diego/prandini/exerciciotecnicoelotech/controller/MockMvcHandler.java
@@ -0,0 +1,29 @@
+package com.diego.prandini.exerciciotecnicoelotech.controller;
+
+import com.diego.prandini.exerciciotecnicoelotech.utils.JsonUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import java.nio.charset.Charset;
+
+public class MockMvcHandler<T> implements ResultHandler {
+
+    private final TypeReference<T> valueTypeRef;
+
+    private T output;
+
+    public MockMvcHandler(TypeReference<T> valueTypeRef) {
+        this.valueTypeRef = valueTypeRef;
+    }
+
+    @Override
+    public void handle(MvcResult result) throws Exception {
+        String json = result.getResponse().getContentAsString(Charset.defaultCharset());
+        output = JsonUtils.fromJson(json, valueTypeRef);
+    }
+
+    public T get() {
+        return output;
+    }
+}
---
+++ src/test/java/com/diego/prandini/exerciciotecnicoelotech/controller/PessoaControllerTest.java
@@ -0,0 +1,62 @@
+package com.diego.prandini.exerciciotecnicoelotech.controller;
+
+import com.diego.prandini.exerciciotecnicoelotech.application.CriarPessoa;
+import com.diego.prandini.exerciciotecnicoelotech.utils.JsonUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class PessoaControllerTest {
+
+    private static final String NOME_DEFAULT = "Joao";
+    private static final String CPF_DEFAULT = "37783132669";
+    private static final LocalDate DATA_DE_NASCIMENTO_DEFAULT = LocalDate.of(1991, Month.NOVEMBER, 25);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deveCriarPessoa() throws Exception {
+        CriarPessoa.Output output = doPostPessoa();
+
+        assertThat(output).isNotNull();
+        assertThat(output.id()).isNotNull();
+        assertThat(output.nome()).isEqualTo(NOME_DEFAULT);
+        assertThat(output.cpf()).isEqualTo(CPF_DEFAULT);
+        assertThat(output.dataDeNascimento()).isEqualTo(DATA_DE_NASCIMENTO_DEFAULT);
+    }
+
+    private CriarPessoa.Output doPostPessoa() throws Exception {
+        MockMvcHandler<CriarPessoa.Output> handler = new MockMvcHandler<>(new TypeReference<>() {
+        });
+        String input = JsonUtils.toJson(new CriarPessoa.Input(
+                NOME_DEFAULT, CPF_DEFAULT, DATA_DE_NASCIMENTO_DEFAULT
+        ));
+        mockMvc.perform(post("/pessoas").contentType(MediaType.APPLICATION_JSON)
+                        .content(input))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(handler);
+        return handler.get();
+    }
+}
