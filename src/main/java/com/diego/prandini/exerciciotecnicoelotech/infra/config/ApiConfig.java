5a60e482c8f59bfc5181ed39ee86d685949cebbb
diego.prandini <contato.diego.prandini@gmail.com>
2023-03-04 12:21:52 -0300

    Liberado cors

--- src/main/java/com/diego/prandini/exerciciotecnicoelotech/infra/config/ApiConfig.java
+++ src/main/java/com/diego/prandini/exerciciotecnicoelotech/infra/config/ApiConfig.java
@@ -8,10 +8,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableConfigurationProperties({ApplicationProperties.class})
-public class ApiConfig {
+public class ApiConfig implements WebMvcConfigurer {
 
     @Bean
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
@@ -25,5 +27,14 @@ public class ApiConfig {
                         .title(applicationProperties.getTitle())
                         .version(applicationProperties.getVersion())
                         .description("exercicio-tecnico-elotech"));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600);
     }
 }
